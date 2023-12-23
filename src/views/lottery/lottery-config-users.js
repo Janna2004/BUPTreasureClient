a922abf56a78018b81810dc364ec63236ede6a6d
A_CS <1692882051@qq.com>
2023-12-23 08:10:04 +0800

    update

--- src/views/lottery/lottery-config-users.js
+++ src/views/lottery/lottery-config-users.js
@@ -1,6 +1,21 @@
-import lotteryConfigUsersRawJson from './lottery-config-users.json';
+let lotteryConfigUsersRawJson;
+
+try {
+  // 尝试导入JSON文件
+  lotteryConfigUsersRawJson = require('./../../buffer/lottery-config-users.json');
+  console.log('lotteryConfigUsersRawJson', lotteryConfigUsersRawJson);
+} catch (error) {
+  // 发生错误时，输出错误信息
+  console.error('Failed to import lottery-config-users.json:', error);
+
+  // 刷新页面
+  setTimeout(() => {
+    window.location.reload();
+  }, 2000);
+}
 
 const cardUserList = lotteryConfigUsersRawJson;
+
 // table模式下行列数
 let row = 1;
 let col = 1;
